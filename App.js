cc223bca65a6bd1e0fb5b5d616caa1ac0b395067
Usama.K98 <i190493@gmail.com>
2022-07-25 16:33:10 +0500

    - second commit

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useCallback, useMemo, useRef, useState, useEffect } from 'react';
 import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
-import { Platform, StyleSheet, Text, View, Dimensions } from 'react-native';
-import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import { Pressable, StyleSheet, Text, View, Dimensions, Button } from 'react-native';
 import {locations} from './source/Data'
 import CustomMarker from './source/components/CustomMarker'
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import * as Location from 'expo-location';
+import { BottomSheet } from 'react-native-btr';
+
+//import to show social icons
+import { SocialIcon } from 'react-native-elements';
+
+let LATITUDE= 33.639935
+let LONGITUDE= 72.9158337
+let LATITUDE_DELTA= 0.0922
+let LONGITUDE_DELTA= 0.0421
+
+//How to get Lat and Long of corners of screen 
+/* {
+  latlng: {
+      latitude: REGION.latitude+(REGION.latitudeDelta/2),
+      longitude: REGION.longitude-(REGION.longitudeDelta/2)
+  },
+  title:'topLeft'
+
+},
+{
+  latlng: {
+      latitude: REGION.latitude-(REGION.latitudeDelta/2),
+      longitude: REGION.longitude+(REGION.longitudeDelta/2)
+  },
+  title:'bottomRight'
+
+} */
 
 export default function App() {
 
-  const sheetRef = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
-  const snapPoints = ['25%', '40%'];
-  const [location, setLocation] = useState('');
-  const [errorMsg, setErrorMsg] = useState(null);
+  const [visible, setVisible] = useState(false);
 
+  const toggleBottomNavigationView = () => {
+    //Toggling the visibility state of the bottom sheet
+    setPin()
+    setVisible(!visible);
+  };
+
+  const modalRef = useRef(null);
+  //const snapPoints = useMemo(() => ['25%', '50%'], []);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  var [pin, setPin] = useState('undefined');
+
+  
+
+  const onOpen = () => {
+    modalRef.current?.open();
+  };
+
+  const onClose = () => {
+    modalRef.current?.close();
+  };
+ 
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -37,17 +79,32 @@ export default function App() {
     console.log(location)
   }
 
+  showHeader = () => (
+    <View style={styles.panelHeader}>
+      <View style={styles.modalHeader}>
+        <View style={styles.panelHandle} />
+        <Text style={styles.modalHeaderText}>
+          Swipe down to close the modal
+        </Text>
+      </View>
+    </View>
+  );
+  showContent = () => (
+    <>
+      <View style={styles.panel}>
+        <Text style={{ marginBottom: 10 }}>
+          Hello World!
+        </Text>
+      </View>
+    </>
+  );
+  
   return (
+    
     <View style={styles.container}>
       <MapView.Animated style={styles.map}
       provider={PROVIDER_GOOGLE}
       showsUserLocation
-      initialRegion = {{ 
-        latitude: 33.639935,
-        longitude: 72.9158337,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
       >
       
       {
@@ -55,25 +112,34 @@ export default function App() {
           <Marker
             coordinate = {{latitude: marker.latitude,
             longitude: marker.longitude}}
+            onPress={toggleBottomNavigationView}
+            setPin = {
+              pin = marker.title
+            }
           >
-          <CustomMarker item = {marker} />
+          
+
+          <CustomMarker item = {marker}/>
             <Callout>
-              
+              <Text>Hello</Text>
             </Callout>
           </Marker>
         ))
       }
     </MapView.Animated>
 
-      <BottomSheet
-          ref = {sheetRef}
-          snapPoints = {snapPoints}
-          enablePanDownToClose={true}
-          onClose={() => setIsOpen(false)}
-      >
-        <BottomSheetView>
-          <Text>hello</Text>
-        </BottomSheetView>
+    <Button
+          onPress={toggleBottomNavigationView}
+          title="Show Bottom Sheet"
+        />
+        <BottomSheet
+          visible={visible}
+          onBackButtonPress={toggleBottomNavigationView}
+          onBackdropPress={toggleBottomNavigationView}
+        >
+        <View style={styles.container}>
+          <Text>{pin}</Text>
+        </View>
       </BottomSheet>
     </View>
   );
@@ -87,7 +153,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   map: {
-    width: Dimensions.get('window').width-50,
-    height: Dimensions.get('window').height-80,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
